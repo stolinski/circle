209fbe820f9124df8df7f3bd775628e766b83767
Scott Tolinski <scott.tolinski@gmail.com>
2025-05-14 10:00:46 -0600

    completes app

--- src/Svelte.svelte
+++ src/Svelte.svelte
@@ -80,8 +80,14 @@
     background-size: 50%, cover;
     width: 300px;
     margin: 0 auto;
-    border-radius: 5px;
+    border-radius: 8px;
     height: 300px;
+    border: 1px solid #bbb;
+    box-shadow:
+      0 20px 25px -5px rgb(0 0 0 / 0.2),
+      0 10px 15px -3px rgb(0 0 0 / 0.15),
+      0 4px 6px -4px rgb(0 0 0 / 0.1),
+      0 2px 4px rgb(0 0 0 / 0.1);
   }
 
   .controls {
@@ -101,6 +107,9 @@
     font-size: 1.5rem;
     cursor: pointer;
     transition: all 0.2s ease;
+    box-shadow:
+      0 4px 6px -1px rgb(0 0 0 / 0.2),
+      0 2px 4px -2px rgb(0 0 0 / 0.1);
   }
 
   .controls button:disabled {

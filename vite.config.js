ab30203e6fd69d0d682412acd683760d39900831
Scott Tolinski <scott.tolinski@gmail.com>
2025-05-14 09:28:22 -0600

    fixes vite conf

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [svelte()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        sveltePage: resolve(__dirname, "svelte.html"),
+      },
+    },
+  },
 });
